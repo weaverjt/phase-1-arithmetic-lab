c4855c64723047e856cbd8b77c7b7d191421bbe2
Jacob <weaver.jacob.t@gmail.com>
2023-09-01 20:54:20 -0500

    math is dumb

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+var num1 = 2;
+var num2 = 31;
 
-// Write your code here
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 10;
+var num4 = 6;
+
+const mod = num3 - num4;
+
+var max = Math.max(1, 2, 3, 4, 5, 20);
